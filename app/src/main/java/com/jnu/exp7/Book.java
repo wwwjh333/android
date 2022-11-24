ac1c068341199dd617e87ba93f46889575ed907c
wwwjh333 <948794017@qq.com>
2022-11-24 17:11:34 +0800

    new

--- app/src/main/java/com/jnu/exp7/Book.java
+++ app/src/main/java/com/jnu/exp7/Book.java
@@ -1,7 +1,19 @@
 package com.jnu.exp7;
 
+
 public class Book {
     public String title; // 标题
-    public String content; // 内容
+    public int CoverResourceId; // 内容
+    Book(String title,int id)
+    {
+        this.setTitle(title);
+        this.setCoverResourceId(id);
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public void setCoverResourceId(int id){
+        this.CoverResourceId = id;
+    }
 }
 
--- app/src/main/java/com/jnu/exp7/MainActivity.java
+++ app/src/main/java/com/jnu/exp7/MainActivity.java
@@ -6,21 +6,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.ContextMenu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class MainActivity extends AppCompatActivity {
     RecyclerView mRecyclerView;
-    MyAdapter mMyAdapter ;
-    Boolean up = false;
-    List<Book> mBookList = new ArrayList<>();
+    MyAdapter mMyAdapter;
+    Book book1 = new Book("软件项目管理案例教程（第4版）",R.drawable.book_2);
+    Book book2 = new Book("创新工程实践",R.drawable.book_no_name);
+    Book book3 = new Book("信息安全数学基础（第2版）",R.drawable.book_1);
+    List<Book> mBookList = new ArrayList<Book>(){{
+        add(book1);
+        add(book2);
+        add(book3);
+    }};
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,48 +38,32 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mRecyclerView = findViewById(R.id.recyclerview);
         registerForContextMenu(mRecyclerView);
-        // 构造一些数据
-        for (int i = 0; i < 10; i++) {
-            Book book = new Book();
-            book.title = "图书名称" + i;
-            book.content = "图书内容" + i;
-            mBookList.add(book);
-        }
         mMyAdapter = new MyAdapter();
         mRecyclerView.setAdapter(mMyAdapter);
         LinearLayoutManager layoutManager = new LinearLayoutManager(MainActivity.this);
         mRecyclerView.setLayoutManager(layoutManager);
     }
 
-    @Override
-    public void onPause() {
-        super.onPause();
-        up = true;//不可见的时候将刷新开启
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        if (up) {
-            up = false;//刷新一次即可，不需要一直刷新
-        }
-    }
 
 
     @Override
     public boolean onContextItemSelected(MenuItem item) {
         int position;
-        position =mMyAdapter.getContextMenuPosition();
-        switch (item.getItemId()) {
-            case 0:
-                mMyAdapter.delete(position);
-            case 1:
-                break;
-            default:
-                break;
+        position = mMyAdapter.getContextMenuPosition();
+        if (item.getItemId() == 0){
+            mMyAdapter.delete(position);
+            mRecyclerView.setAdapter(mMyAdapter);
+            LinearLayoutManager layoutManager = new LinearLayoutManager(MainActivity.this);
+            mRecyclerView.setLayoutManager(layoutManager);
+       }
+        else if(item.getItemId()==1) {
+            Intent intent = new Intent(MainActivity.this, com.jnu.exp7.EditBookActivity.class);
+            startActivity(intent);
         }
+
         return super.onContextItemSelected(item);
     }
+
 
 
     public  class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHoder> {
@@ -97,13 +91,10 @@ public class MainActivity extends AppCompatActivity {
         public void onBindViewHolder(final MyAdapter.MyViewHoder holder, int position) {
             Book books = mBookList.get(position);
             holder.mTitleTv.setText(books.title);
-            holder.mTitleContent.setText(books.content);
-            holder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
-                @Override
-                public boolean onLongClick(View v) {
-                    setContextMenuPosition(holder.getLayoutPosition());
-                    return false;
-                }
+            holder.mImg.setImageDrawable(getResources().getDrawable(books.CoverResourceId));
+            holder.itemView.setOnLongClickListener(v -> {
+                setContextMenuPosition(holder.getLayoutPosition());
+                return false;
             });
         }
 
@@ -117,14 +108,18 @@ public class MainActivity extends AppCompatActivity {
             mBookList.remove(position);
         }
 
+        public void edit(int position,String bookName){
+            mBookList.get(position).setTitle(bookName);
+        }
+
         class MyViewHoder extends RecyclerView.ViewHolder implements View.OnCreateContextMenuListener{
             TextView mTitleTv;
-            TextView mTitleContent;
+            ImageView mImg;
 
             public MyViewHoder(@NonNull View itemView) {
                 super(itemView);
                 mTitleTv = itemView.findViewById(R.id.title);
-                mTitleContent = itemView.findViewById(R.id.content);
+                mImg = itemView.findViewById(R.id.img);
                 itemView.setOnCreateContextMenuListener(this);
             }
 
@@ -137,6 +132,8 @@ public class MainActivity extends AppCompatActivity {
 
         }
     }
+
+
 
 }
 
